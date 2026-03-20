6417d9bda6c968828995fbe37d3a778bc8814977
AttiliaTheHun <picek.jaroslav@protonmail.com>
2026-03-20 16:00:47 +0100

    Fix images, add favicon

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,12 +3,9 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default {
   site: 'https://elephantslife.eu',
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare(),
   output: 'static'
 };
